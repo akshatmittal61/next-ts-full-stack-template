433dff1c6e0743f6f8b1807d1ed732d824af0163
Akshat Mittal <akshatmittal2506@gmail.com>
2025-01-24 17:39:03 +0530

    feat: added logger

--- config/variables.ts
+++ config/variables.ts
@@ -1,4 +1,5 @@
 type T_URL = "frontend" | "backend";
+type T_NODE_ENV = "development" | "production" | "test";
 
 export const url: Record<T_URL, string> = {
 	frontend:
@@ -7,5 +8,8 @@ export const url: Record<T_URL, string> = {
 		process.env.NEXT_PUBLIC_BACKEND_BASE_URL ||
 		"http://localhost:3000/api/v1",
 };
+
+export const nodeEnv: T_NODE_ENV =
+	process.env.NODE_ENV || process.env.NEXT_PUBLIC_NODE_ENV || "production";
 
 export const service = process.env.NEXT_PUBLIC_SERVICE || "template";
---
+++ log/index.ts
@@ -0,0 +1,124 @@
+/* eslint-disable no-console */
+import { nodeEnv } from "@/config";
+import { serviceName } from "@/constants";
+
+type LOG_LEVEL =
+	| "log"
+	| "info"
+	| "warn"
+	| "error"
+	| "debug"
+	| "verbose"
+	| "silly"
+	| "http";
+
+export class Logger {
+	private static getTimestamp() {
+		const date = new Date();
+		return date.toISOString();
+	}
+	private static getLevel(level: LOG_LEVEL) {
+		return level.toUpperCase();
+	}
+	private static getConsoleColor(level: LOG_LEVEL) {
+		switch (level) {
+			case "info":
+				return "\x1b[32m%s\x1b[37m";
+			case "warn":
+				return "\x1b[33m%s\x1b[0m";
+			case "error":
+				return "\x1b[31m%s\x1b[0m";
+			case "debug":
+				return "\x1b[34m%s\x1b[0m";
+			case "verbose":
+				return "\x1b[35m%s\x1b[0m";
+			case "silly":
+				return "\x1b[36m%s\x1b[0m";
+			case "http":
+				return "\x1b[35m%s\x1b[0m";
+			default:
+				return "\x1b[37m%s\x1b[0m";
+		}
+	}
+	private static getConsoleMethod(level: LOG_LEVEL) {
+		switch (level) {
+			case "info":
+				return console.info;
+			case "warn":
+				return console.warn;
+			case "error":
+				return console.error;
+			case "debug":
+				return console.debug;
+			case "verbose":
+				return console.log;
+			case "silly":
+				return console.log;
+			case "http":
+				return console.log;
+			default:
+				return console.log;
+		}
+	}
+	private static getMessage(...messages: Array<any>): string {
+		const message = messages
+			.map((m) => {
+				if (m === null) return "null";
+				if (m === undefined) return "undefined";
+				if (typeof m === "string") return `"${m}"`;
+				if (typeof m === "object") return JSON.stringify(m);
+				else return m;
+			})
+			.map((m) => m.toString());
+		return `${message}`;
+	}
+	private static getMessageToLog(level: LOG_LEVEL, ...messages: Array<any>) {
+		const timestamp = Logger.getTimestamp();
+		const logLevel = Logger.getLevel(level);
+		const message = Logger.getMessage(...messages);
+		const service = `${serviceName}-${nodeEnv}`;
+		const messageToLog = `[${service}] [${timestamp}] [${logLevel}] [${message}]\n`;
+		return messageToLog;
+	}
+	private static writeToConsole(level: LOG_LEVEL, message: string) {
+		const color = Logger.getConsoleColor(level);
+		const method = Logger.getConsoleMethod(level);
+		method(color, message);
+	}
+	private static logMessages = (level: LOG_LEVEL, messages: Array<any>) => {
+		const message = Logger.getMessageToLog(level, ...messages);
+		Logger.writeToConsole(level, message);
+	};
+
+	public static info(...messages: Array<any>) {
+		Logger.logMessages("info", messages);
+	}
+
+	public static warn(...messages: Array<any>) {
+		Logger.logMessages("warn", messages);
+	}
+
+	public static error(...messages: Array<any>) {
+		Logger.logMessages("error", messages);
+	}
+
+	public static debug(...messages: Array<any>) {
+		Logger.logMessages("debug", messages);
+	}
+
+	public static verbose(...messages: Array<any>) {
+		Logger.logMessages("verbose", messages);
+	}
+
+	public static silly(...messages: Array<any>) {
+		Logger.logMessages("silly", messages);
+	}
+
+	public static http(...messages: Array<any>) {
+		Logger.logMessages("http", messages);
+	}
+
+	public static log(...messages: Array<any>) {
+		Logger.logMessages("log", messages);
+	}
+}
