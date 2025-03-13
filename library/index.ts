4c76f29686f2a245b9008192856d1238995d7e27
Akshat Mittal <akshatmittal2506@gmail.com>
2025-03-14 01:47:54 +0530

    feat: added overlay widgets

--- library/index.ts
+++ library/index.ts
@@ -1,5 +1,7 @@
+export * from "./Avatar";
 export { default as Button } from "./Button";
 export { default as IconButton } from "./Button/icon";
-export * from "./Typography";
-export * from "./Avatar";
 export * from "./icons";
+export * from "./Pane";
+export * from "./Popup";
+export * from "./Typography";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,50 @@
-import { Button, Typography } from "@/library";
-import React from "react";
+import { Button, Pane, Popup, Typography } from "@/library";
+import { Logger } from "@/log";
+import React, { useState } from "react";
 import { FiAlertTriangle } from "react-icons/fi";
 
 const HomePage: React.FC = () => {
+	const [openPopup, setOpenPopup] = useState(false);
+	const [openPane, setOpenPane] = useState(false);
 	return (
-		<main className="w-screen h-screen flex justify-center items-center flex-col gap-4">
-			<Typography
-				className="text-blue-600"
-				as="h1"
-				size="head-1"
-				weight="bold"
-			>
-				Home Page
-			</Typography>
-			<Button
-				onClick={() => {
-					throw new Error("Just for Error Boundary");
-				}}
-				size="large"
-				theme="error"
-				icon={<FiAlertTriangle />}
-			>
-				Throw an Error
-			</Button>
-		</main>
+		<>
+			<main className="w-screen h-screen flex justify-center items-center flex-col gap-4">
+				<Typography
+					className="text-blue-600"
+					as="h1"
+					size="head-1"
+					weight="bold"
+				>
+					Home Page
+				</Typography>
+				<Button
+					onClick={() => {
+						throw new Error("Just for Error Boundary");
+					}}
+					size="large"
+					theme="error"
+					icon={<FiAlertTriangle />}
+				>
+					Throw an Error
+				</Button>
+				<Button onClick={() => setOpenPopup(true)}>Open Popup</Button>
+				<Button onClick={() => setOpenPane(true)}>Open Pane</Button>
+			</main>
+			{openPopup ? (
+				<Popup
+					onEdit={() => Logger.info("Edit")}
+					onDelete={() => Logger.info("Delete")}
+					onClose={() => setOpenPopup(false)}
+				>
+					You are in a popup
+				</Popup>
+			) : null}
+			{openPane ? (
+				<Pane title="Pane Title" onClose={() => setOpenPane(false)}>
+					You are in a pane
+				</Pane>
+			) : null}
+		</>
 	);
 };
 
