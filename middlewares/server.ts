df26d86ee1f32722d942846979e04e75c216601d
Akshat Mittal <akshatmittal2506@gmail.com>
2025-05-10 23:30:24 +0530

    fix: minor fixes comments

--- middlewares/server.ts
+++ middlewares/server.ts
@@ -1,4 +1,4 @@
-import { HTTP } from "@/constants";
+import { HTTP, USER_ROLE } from "@/constants";
 import { Logger } from "@/log";
 import { ApiController, ApiRequest, ApiResponse } from "@/types";
 import { ApiFailure, genericParse, getNonEmptyString } from "@/utils";
@@ -7,6 +7,7 @@ export class ServerMiddleware {
 	public static authenticatedRoute(next: ApiController): ApiController {
 		return async (req: ApiRequest, res: ApiResponse) => {
 			try {
+				// Update with your authentication logic
 				const token = genericParse(
 					getNonEmptyString,
 					req.cookies.token
@@ -17,7 +18,7 @@ export class ServerMiddleware {
 				return next(req, res);
 			} catch (error) {
 				Logger.error(error);
-				return ApiFailure(res).send(
+				return new ApiFailure(res).send(
 					HTTP.message.UNAUTHORIZED,
 					HTTP.status.UNAUTHORIZED
 				);
@@ -27,14 +28,16 @@ export class ServerMiddleware {
 	public static adminRoute(next: ApiController): ApiController {
 		return async (req: ApiRequest, res: ApiResponse) => {
 			try {
+				// Update with your authentication logic
+				// Check if the user is an admin
 				const role = genericParse(getNonEmptyString, req.cookies.role);
-				if (role !== "admin") {
+				if (role !== USER_ROLE.ADMIN) {
 					throw new Error("Not an admin");
 				}
 				return next(req, res);
 			} catch (error) {
 				Logger.error(error);
-				return ApiFailure(res).send(
+				return new ApiFailure(res).send(
 					HTTP.message.FORBIDDEN,
 					HTTP.status.FORBIDDEN
 				);
