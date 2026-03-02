c584ddb2827ba731a15d00e48f57e494711c4715
Akshat Mittal <akshatmittal2506@gmail.com>
2026-03-02 19:32:45 +0530

    feat: added head tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<meta charSet="utf-8" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link
 					rel="preconnect"
@@ -22,6 +23,14 @@ export default function Document() {
 					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=optional"
 					rel="stylesheet"
 				/>
+				<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+				<link rel="shortcut icon" href="/favicon.ico" />
+				<link rel="manifest" href="/manifest.json" />
+				<meta
+					name="viewport"
+					content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+				/>
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 			</Head>
 			<body className="antialiased">
 				<Main />
